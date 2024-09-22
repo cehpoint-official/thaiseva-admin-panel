2897ac4c355f919c329d9a8d74003cc94fe91905
Dwaipayan Biswas <biswastatay73@gmail.com>
2024-09-22 12:54:39 +0530

    Update the errors

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({
+      open: true, // Automatically open the visualizer report in the browser
+      filename: "stats.html", // Output file for the visualizer report
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Example of splitting external libraries into separate chunks
+          "react-vendor": ["react", "react-dom"],
+          "utility-vendor": ["lodash", "moment"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Set chunk size warning limit to 1000 KB
+  },
+});
