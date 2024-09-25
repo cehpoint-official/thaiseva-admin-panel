895d98542e12cf3e40e7e127dc728ba6339f42f6
Arnab Bhattacharyya <arnabbhattacharyya6294@gmail.com>
2024-09-25 22:55:30 +0530

    index page

--- src/restaurantAdmin/component/AuthPage.jsx
+++ src/restaurantAdmin/component/AuthPage.jsx
@@ -1,163 +1,224 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { auth, db } from '../../../firebaseConfig';  // Import Firestore config
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import { doc, setDoc } from 'firebase/firestore';  // Import Firestore methods
-import { FaGoogle, FaEnvelope, FaLock, FaExclamationCircle } from 'react-icons/fa';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, db } from "../../../firebaseConfig"; // Import Firestore config
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore"; // Import Firestore methods
+import {
+  FaGoogle,
+  FaEnvelope,
+  FaLock,
+  FaExclamationCircle,
+} from "react-icons/fa";
 
 const AuthPageAdmin = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [name, setName] = useState('');  // Added name field
-    const [isSignUp, setIsSignUp] = useState(false);
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState(""); // Added name field
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-    const handleEmailChange = (e) => setEmail(e.target.value);
-    const handlePasswordChange = (e) => setPassword(e.target.value);
-    const handleNameChange = (e) => setName(e.target.value);  // Handle name change
-    const toggleAuthMode = () => setIsSignUp((prev) => !prev);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const handleNameChange = (e) => setName(e.target.value); // Handle name change
+  const toggleAuthMode = () => setIsSignUp((prev) => !prev);
 
-    // Function to store user details in Firestore with default values
-    const storeUserInFirestore = async (user) => {
-        await setDoc(doc(db, 'users', user.uid), {
-            name: name,  // Name of the user
-            email: user.email,
-            isUser: true,  // Default isUser to true
-            isRestaurantAdmin: false,  // Default isRestaurantAdmin to false
-            isMasterAdmin: false,  // Default isMasterAdmin to false
-            createdAt: new Date(),
-            userId: user.uid,
-        });
-    };
+  // Function to store user details in Firestore with default values
+  const storeUserInFirestore = async (user) => {
+    await setDoc(doc(db, "users", user.uid), {
+      name: name, // Name of the user
+      email: user.email,
+      isUser: true, // Default isUser to true
+      isRestaurantAdmin: false, // Default isRestaurantAdmin to false
+      isMasterAdmin: false, // Default isMasterAdmin to false
+      createdAt: new Date(),
+      userId: user.uid,
+    });
+  };
 
-    const handleEmailPasswordAuth = async () => {
-        try {
-            setError('');
-            if (isSignUp) {
-                // Sign up user
-                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-                const user = userCredential.user;
+  const handleEmailPasswordAuth = async () => {
+    try {
+      setError("");
+      if (isSignUp) {
+        // Sign up user
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const user = userCredential.user;
 
-                // Store default user data in Firestore after sign-up
-                await storeUserInFirestore(user);
-            } else {
-                // Sign in user
-                await signInWithEmailAndPassword(auth, email, password);
-            }
-            navigate('/');
-        } catch (error) {
-            setError(error.message);
-        }
-    };
+        // Store default user data in Firestore after sign-up
+        await storeUserInFirestore(user);
 
-    const handleGoogleAuth = async () => {
-        const provider = new GoogleAuthProvider();
-        try {
-            setError('');
-            const result = await signInWithPopup(auth, provider);
-            const user = result.user;
+        // Fetch user data after successful sign-up
+        await fetchUserData(user.uid);
+      } else {
+        // Sign in user
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const user = userCredential.user;
 
-            // Store default user data after Google sign-in
-            await storeUserInFirestore(user);
-            navigate(`/restaurantAdmin/${userId}`);
-        } catch (error) {
-            setError(error.message);
-        }
-    };
+        // Fetch user data after successful sign-in
+        await fetchUserData(user.uid);
+      }
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-purple-100">
-            <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
-                <div className="text-center mb-8">
-                    <h1 className="text-4xl font-bold text-gray-800">Thaiseva</h1>
-                    <h2 className="text-2xl font-semibold text-gray-600 mt-2">{isSignUp ? 'Sign Up' : 'Sign In'}</h2>
-                </div>
-                <form onSubmit={(e) => e.preventDefault()} className="space-y-6">
-                    {isSignUp && (  // Show name field only during sign-up
-                        <div>
-                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-                                Name
-                            </label>
-                            <div className="flex items-center border-2 rounded-lg px-3 py-2 focus-within:border-blue-500 transition duration-300">
-                                <input
-                                    type="text"
-                                    id="name"
-                                    value={name}
-                                    onChange={handleNameChange}
-                                    className="w-full focus:outline-none"
-                                    placeholder="Enter your name"
-                                    required
-                                />
-                            </div>
-                        </div>
-                    )}
-                    <div>
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                            Email
-                        </label>
-                        <div className="flex items-center border-2 rounded-lg px-3 py-2 focus-within:border-blue-500 transition duration-300">
-                            <FaEnvelope className="text-gray-400 mr-2" />
-                            <input
-                                type="email"
-                                id="email"
-                                value={email}
-                                onChange={handleEmailChange}
-                                className="w-full focus:outline-none"
-                                placeholder="Enter your email"
-                                required
-                            />
-                        </div>
-                    </div>
-                    <div>
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                            Password
-                        </label>
-                        <div className="flex items-center border-2 rounded-lg px-3 py-2 focus-within:border-blue-500 transition duration-300">
-                            <FaLock className="text-gray-400 mr-2" />
-                            <input
-                                type="password"
-                                id="password"
-                                value={password}
-                                onChange={handlePasswordChange}
-                                className="w-full focus:outline-none"
-                                placeholder="Enter your password"
-                                required
-                            />
-                        </div>
-                    </div>
-                    {error && (
-                        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                            <FaExclamationCircle className="inline mr-2" />
-                            <span className="block sm:inline">{error}</span>
-                        </div>
-                    )}
-                    <button
-                        onClick={handleEmailPasswordAuth}
-                        className="w-full bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-3 px-4 rounded-lg focus:outline-none focus:shadow-outline transition duration-300"
-                    >
-                        {isSignUp ? 'Sign Up' : 'Sign In'}
-                    </button>
-                </form>
-                <div className="mt-6">
-                    <button
-                        onClick={handleGoogleAuth}
-                        className="w-full bg-white border border-gray-300 text-gray-700 font-bold py-3 px-4 rounded-lg flex items-center justify-center hover:bg-gray-50 focus:outline-none focus:shadow-outline transition duration-300"
-                    >
-                        <FaGoogle className="mr-2 text-red-500" /> Sign In with Google
-                    </button>
-                </div>
-                <div className="mt-6 text-center">
-                    <button
-                        onClick={toggleAuthMode}
-                        className="text-blue-500 hover:text-blue-600 text-sm focus:outline-none focus:underline transition duration-300"
-                    >
-                        {isSignUp ? 'Already have an account? Sign In' : 'Need an account? Sign Up'}
-                    </button>
-                </div>
-            </div>
+  const handleGoogleAuth = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      setError("");
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // Store default user data after Google sign-in
+      await storeUserInFirestore(user);
+
+      // Fetch user data after Google sign-in
+      await fetchUserData(user.uid);
+
+      navigate(`/restaurantAdmin/${user.uid}`);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  // Fetch user data from Firestore
+  const fetchUserData = async (userId) => {
+    try {
+      const docRef = doc(db, "users", userId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("User data:", docSnap.data());
+        // Handle the fetched data (e.g., set state or route user based on role)
+      } else {
+        console.log("No such user!");
+      }
+    } catch (error) {
+      console.error("Error fetching user data: ", error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-purple-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-gray-800">Thaiseva</h1>
+          <h2 className="text-2xl font-semibold text-gray-600 mt-2">
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </h2>
         </div>
-    );
+        <form onSubmit={(e) => e.preventDefault()} className="space-y-6">
+          {isSignUp && ( // Show name field only during sign-up
+            <div>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <div className="flex items-center border-2 rounded-lg px-3 py-2 focus-within:border-blue-500 transition duration-300">
+                <input
+                  type="text"
+                  id="name"
+                  value={name}
+                  onChange={handleNameChange}
+                  className="w-full focus:outline-none"
+                  placeholder="Enter your name"
+                  required
+                />
+              </div>
+            </div>
+          )}
+          <div>
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <div className="flex items-center border-2 rounded-lg px-3 py-2 focus-within:border-blue-500 transition duration-300">
+              <FaEnvelope className="text-gray-400 mr-2" />
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+                className="w-full focus:outline-none"
+                placeholder="Enter your email"
+                required
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <div className="flex items-center border-2 rounded-lg px-3 py-2 focus-within:border-blue-500 transition duration-300">
+              <FaLock className="text-gray-400 mr-2" />
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+                className="w-full focus:outline-none"
+                placeholder="Enter your password"
+                required
+              />
+            </div>
+          </div>
+          {error && (
+            <div
+              className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+              role="alert"
+            >
+              <FaExclamationCircle className="inline mr-2" />
+              <span className="block sm:inline">{error}</span>
+            </div>
+          )}
+          <button
+            onClick={handleEmailPasswordAuth}
+            className="w-full bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-bold py-3 px-4 rounded-lg focus:outline-none focus:shadow-outline transition duration-300"
+          >
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </button>
+        </form>
+        <div className="mt-6">
+          <button
+            onClick={handleGoogleAuth}
+            className="w-full bg-white border border-gray-300 text-gray-700 font-bold py-3 px-4 rounded-lg flex items-center justify-center hover:bg-gray-50 focus:outline-none focus:shadow-outline transition duration-300"
+          >
+            <FaGoogle className="mr-2 text-red-500" /> Sign In with Google
+          </button>
+        </div>
+        <div className="mt-6 text-center">
+          <button
+            onClick={toggleAuthMode}
+            className="text-blue-500 hover:text-blue-600 text-sm focus:outline-none focus:underline transition duration-300"
+          >
+            {isSignUp
+              ? "Already have an account? Sign In"
+              : "Need an account? Sign Up"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthPageAdmin;
--- src/restaurantAdmin/pages/Home.jsx
+++ src/restaurantAdmin/pages/Home.jsx
@@ -1,13 +1,174 @@
-import { useState } from "react";
-import Dashboard from "../component/Dashboard";
-import Sidebar from "../component/Sidebar";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../firebaseConfig";
+import { useAuth } from "../../AuthContext";
 
 const Home = () => {
-  const [show, setShow] = useState(window.innerWidth >= 600 ? true : false);
-  const toggle = (e) => setShow(!show);
+  const [show, setShow] = useState(false);
+  const toggle = () => setShow((prev) => !prev); // Toggles the modal visibility
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
+
+  const handleLogOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/auth");
+    } catch (error) {
+      console.error("Error logging out: ", error);
+    }
+  };
+
+  // Close modal when clicking outside of it
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (show && !event.target.closest(".modal")) {
+        setShow(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [show]);
+
   return (
-    <div className='flex items-start bg-black w-screen h-screen'>
-      Home
+    <div className=" w-screen h-screen flex p-12 bg-gray-900">
+      {/* Left side: Notes */}
+      <div className="relative bg-yellow-100 text-black rounded-md w-[40%] h-full flex flex-col justify-center p-6">
+        {/* Ping Circle */}
+        <div className="absolute -top-2 -right-2">
+          <div className="relative">
+            <span className="flex h-6 w-6">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-6 w-6 bg-red-500"></span>
+            </span>
+          </div>
+        </div>
+
+        <h3 className="font-bold mb-2">Note for Visitors:</h3>
+        <ul className="list-disc ml-6">
+          <li>
+            <span className="font-bold">
+              This page is just for demo purposes
+            </span>{" "}
+            and is not completed yet.
+          </li>
+          <li>
+            The <span className="font-bold">"Restaurant Admin"</span> and{" "}
+            <span className="font-bold">"Food Admin"</span> buttons are
+            temporarily placed here for development and testing purposes and
+            will not be visible on the final page.
+          </li>
+          <li>
+            Navigation to the{" "}
+            <span className="font-bold">"Restaurant Admin"</span> page is
+            allowed if your role in the <strong>users</strong> database is
+            either:
+            <ul className="list-decimal ml-6">
+              <li>
+                <span className="font-bold">Role = Partner</span>
+              </li>
+              <li>
+                <span className="font-bold">isRestaurantAdmin = true</span>
+              </li>
+            </ul>
+          </li>
+          <li>
+            Navigation to the <span className="font-bold">"Food Admin"</span>{" "}
+            page is allowed if your role in the <strong>users</strong> database
+            is either:
+            <ul className="list-decimal ml-6">
+              <li>
+                <span className="font-bold">Role = Admin</span>
+              </li>
+              <li>
+                <span className="font-bold">isMasterAdmin = true</span>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <span className="font-bold">
+              Currently, both the role and admin type are being checked.
+            </span>{" "}
+            If there are more logic conditions for accessing these sections,
+            they have not been implemented yet.
+          </li>
+          <li>
+            <span className="animate-pulse text-red-600 font-bold">
+              Important:
+            </span>{" "}
+            For demo purposes, you can use the following credentials to test
+            both Restaurant Admin and Food Admin sections:
+            <p>
+              <strong>*</strong> Email: <strong>arn@gmail.com</strong>
+            </p>
+            <p>
+              <strong>*</strong> Password: <strong>arn@gmail.com</strong>
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      {/* Right side: User details and buttons */}
+      <div className=" text-white p-10 w-[60%] flex flex-col items-center justify-center gap-8">
+        <span className="flex items-center justify-center flex-col">
+          <h2>Your details: </h2>
+          <span className="border-gray-400 border px-5 py-2 rounded-md">
+            <p>
+              <strong>User Email:</strong> {currentUser.email}
+            </p>
+            <p>
+              <strong>UID :</strong> {currentUser.uid}
+            </p>
+          </span>
+        </span>
+
+        {/* Links for testing purposes */}
+        <Link
+          to={`restaurantAdmin/${currentUser.uid}`}
+          className="border-2 text-white w-48 py-2 flex items-center justify-center rounded-full bg-blue-600"
+        >
+          Restaurant Admin
+        </Link>
+
+        <Link
+          to={`foodAdmin`}
+          className="border-2 text-white w-48 py-2 flex items-center justify-center rounded-full bg-blue-600"
+        >
+          Food Admin
+        </Link>
+
+        <button onClick={toggle} className="bg-red-500 px-4 py-2 rounded-full">
+          Logout
+        </button>
+
+        {show && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white h-[20rem] w-[28rem] rounded-xl text-center p-10 modal">
+              <i className="bi bi-box-arrow-right text-5xl text-white mb-4 font-bold bg-[#ff2d2d] p-2 px-4 rounded-full"></i>
+              <p className="text-3xl mt-8 mb-12 text-blue-900 font-semibold">
+                Are you sure you want to log out?
+              </p>
+              <div className="flex gap-2 items-center justify-center">
+                <button
+                  onClick={() => setShow(false)} // Close the modal on "Cancel"
+                  className="p-2 w-24 text-lg text-white font-semibold rounded-lg bg-slate-600"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={handleLogOut}
+                  className="p-2 w-24 text-lg text-white font-semibold rounded-lg bg-blue-600"
+                >
+                  Log out
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
